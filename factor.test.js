bec5ebe573c300190471abd96a691bd4ecad7aaf
Onitsiky RANAIVOSON <hei.onitsiky@gmail.com>
2022-02-02 22:47:46 +0300

    Factor test last version

--- factor.test.js
+++ factor.test.js
@@ -4,18 +4,23 @@ import factor from "./factor.js";
 
 
 describe("facteur de mot", () => {
-    it("should return AUCUN FACTEUR", () => {
-        expect(factor([["A","B"]])).to.be.equals("AUCUN FACTEUR")
-    })
     it("should return BABA", () => {
-        let result = [["AKDEIBABA PARLBABA"]]
-        expect(factor(result)).to.be.equals("BABA")
+        let result = [["AKDEIBABA","PARLBABA"]]
+        expect(factor(result)).to.be.equals("BABA");
+    })
+    it("should return AUCUN FACTEUR", () => {
+        let result = [["A","B"]];
+        expect(factor(result)).to.be.equals("AUCUN FACTEUR");
+    })
+    it("should return AUCUN FACTEUR", () => {
+        let result = [["AZERTY", "QSDFG"]];
+        expect(factor(result)).to.be.equals("AUCUN FACTEUR")
     })
     it("should return an Error", () => {
-        expect(() => factor([[" "]])).to.throw("Error")
+        expect(() => factor([[" "]])).to.throw("Error");
     })
     it("should return an Error", () => {
-        expect(() => factor([[null]])).to.throw("Error")
+        expect(() => factor([[null]])).to.throw("Error");
     })
     it("should return an Error", () => {
         expect(() => factor(isNaN == false)).to.throw("Error")
